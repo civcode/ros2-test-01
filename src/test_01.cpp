c057d922631e5b02e777906dfc861050fe88f9c5
chris <ivi3001@yahoo.de>
2023-10-24 14:52:31 +0200

    not working

--- src/test_01.cpp
+++ src/test_01.cpp
@@ -3,6 +3,16 @@
 #include <cstdio>
 #include <memory>
 
+//#include "rclcpp/qos.hpp"
+#include "std_msgs/msg/string.hpp"
+
+rclcpp::Node::SharedPtr g_node = nullptr;
+
+void subscription_callback(const std_msgs::msg::String::SharedPtr msg) {
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "subscription_callback");  
+  std::cout << "test" << std::endl;
+}
+
 int main(int argc, char ** argv)
 {
   // (void) argc;
@@ -11,11 +21,20 @@ int main(int argc, char ** argv)
   printf("hello world test_pkg_01 package\n");
 
   rclcpp::init(argc, argv);
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("test_01");
+  //std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("test_01");
+  g_node = rclcpp::Node::make_shared("test_01");
+
+  //auto qos = rclcpp::QoS(rclcpp::KeepLast(1), rmw_qos_profile_sensor_data);
+  //auto qos = rclcpp::QoS(rclcpp::QoS::best_effort());
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(1), rmw_qos_profile_sensor_data);
+  //node->create_subscription("/can0/tx/trajectory_data", rclcpp::QoS::best_effort(), )
+  //node->create_subscription<std_msgs::msg::String>("/topic", qos, subscription_callback);
+  //node->create_subscription<std_msgs::msg::String>("/topic", 10, subscription_callback);
+  g_node->create_subscription<std_msgs::msg::String>("topic", 10, subscription_callback);
 
   //node->create_sub
 
-  rclcpp::spin(node);
+  rclcpp::spin(g_node);
   rclcpp::shutdown();
 
   return 0;
